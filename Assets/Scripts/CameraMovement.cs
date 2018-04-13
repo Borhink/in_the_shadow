6c5d2c7164e8f861bbe7b7f213edb3cbeeb53481
qhonore <qhonore@student.42.fr>
2018-04-13 12:23:02 +0200

    first scene

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+	public float startDuration = 20f;
+	public float power = 0.03f;
+	Vector3 originalPos;
+	Vector3 randomDirection;
+	Vector3 dirOrigin;
+	float duration;
+	bool toOrigin = false;
+	Transform board;
+
+	void Start()
+	{
+		duration = startDuration;
+		originalPos = transform.position;
+		randomDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+		board = GameObject.FindWithTag("Board").transform;
+	}
+	void Update()
+	{
+		float speed = Mathf.PingPong(Time.time, startDuration / 4) / (startDuration / 4);
+		if (duration > startDuration / 2)
+		{
+			transform.position += randomDirection * power * Time.deltaTime * speed;
+			transform.LookAt(board);
+			duration -= Time.deltaTime;
+		}
+		else
+		{
+			if (!toOrigin)
+			{
+				dirOrigin = (originalPos - transform.position).normalized;
+				toOrigin = true;
+			}
+			transform.position += dirOrigin * power * Time.deltaTime * speed;
+			transform.LookAt(board);
+			duration -= Time.deltaTime;
+			if (duration <= 0)
+			{
+				randomDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+				duration = startDuration;
+				transform.position = originalPos;
+				toOrigin = false;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	// enum LevelType {RotationX, }
+	public Transform[] objects;
+	public Vector3[] positions;
+	public Vector3[] rotations;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < objects.Length; ++i)
+		{
+			positions[i] = objects[i].position;
+			rotations[i] = objects[i].eulerAngles;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+		{
+			// rotations[i]
+		}
+	}
+}
